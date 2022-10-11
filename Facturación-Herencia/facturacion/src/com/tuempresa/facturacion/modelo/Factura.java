d3a717c29548ca8b0f53dea5b201ddea09c3406c
wilberthechazarreta <114036147+wilberthechazarreta@users.noreply.github.com>
2022-10-10 22:56:30 -0500

    Lección 7 Herencia

---
+++ Facturación-Herencia/facturacion/src/com/tuempresa/facturacion/modelo/Factura.java
@@ -0,0 +1,14 @@
+package com.tuempresa.facturacion.modelo;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Factura extends DocumentoComercial {
+
+	@OneToMany(mappedBy = "factura")
+	Collection<Pedido>pedidos;
+}
