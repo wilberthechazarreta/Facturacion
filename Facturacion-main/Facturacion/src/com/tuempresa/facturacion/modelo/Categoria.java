58897325550303239f2deb3f39fcbba1c5cc8e90
wilberthechazarreta <114036147+wilberthechazarreta@users.noreply.github.com>
2022-10-10 23:48:23 -0500

    Lección 8 Herencia completado

---
+++ Facturacion-main/Facturacion/src/com/tuempresa/facturacion/modelo/Categoria.java
@@ -0,0 +1,12 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+
+import lombok.*;
+@Entity @Getter @Setter
+public class Categoria extends Identificable{
+	
+	 @Column(length=50)
+	 String descripcion;
+	 
+}
---
+++ Facturacion-main/Facturacion/src/com/tuempresa/facturacion/modelo/Detalle.java
@@ -0,0 +1,14 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Embeddable @Getter @Setter
+public class Detalle {
+
+	int cantidad; 
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	Producto producto;
+}
